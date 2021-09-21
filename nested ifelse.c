3bf13a6389ef64525da0721b023645092a7389ac
Abhay Saini <90948162+abhaycodee@users.noreply.github.com>
2021-09-21 16:03:03 +0530

    Add files via upload

---
+++ nested ifelse.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	
+int var1,var2;
+printf("enter the vaue of variable 1\n");
+scanf("%d",&var1);
+printf("enter the value of var2\n");
+scanf("%d",&var2);
+if(var1!=var2){printf("variable 1 is not equal to var2\n");
+
+if(var1>var2){printf("variable1 is greater than variable2\n");}
+
+
+
+
+else{printf("variable2 is greater than variable 1");
+}
+}
+else{printf("variable 1 is equall to variable2");
+}
+	
+	
+	
+}
